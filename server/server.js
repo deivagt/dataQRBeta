56724c53a74e12787ae5309311802489fe53e715
David Belisle <david1alberto@gmail.com>
2023-04-25 12:58:28 -0600

    prueba

--- server/server.js
+++ server/server.js
@@ -5,31 +5,29 @@ const cors = require("cors");
 const workers = require("./routes/workers")
 const port = process.env.PORT || 5000;
 
+
+
 //////
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api", workers);
 
-app.get("/", (req,res)=>{
-  res.send('Api is running')
-})
+/*
 mongoose
   .connect(process.env.ATLAS_URI)
   .then(() => console.log("Connected to database"))
   .catch((error) => console.error(error))
 
-mongoose.connection.on('open', function () {
-  mongoose.connection.db.listCollections(function (error, names) {
-    if (error) {
-      throw new Error(error);
-    } else {
-      names.map(function (name) {
-        console.log('found collection %s', name);
-      });
-    }
-  });
-});
+
+*/
+
+app.get("/", (req,res)=>{
+  res.send('Api is running')
+})
+
+/*
+app.use("/api", workers);
+*/
 
 
 app.listen(port, () => {
