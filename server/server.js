a489157aa9915260be70f4bf10a237548d35a4fc
David Belisle <david1alberto@gmail.com>
2023-04-25 12:41:14 -0600

    added build command

--- server/server.js
+++ server/server.js
@@ -1,38 +1,36 @@
+require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
 const cors = require("cors");
-
-//////
 const workers = require("./routes/workers")
-app.use(cors());
-app.use(express.json());
-require("dotenv").config({ path: "./config.env" });
-app.use("/api",workers);
 const port = process.env.PORT || 5000;
 
+//////
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api", workers);
 
-
-
-
-
+app.get("/", (req,res)=>{
+  res.send('Api is running')
+})
 mongoose
   .connect(process.env.ATLAS_URI)
   .then(() => console.log("Connected to database"))
   .catch((error) => console.error(error))
 
-  mongoose.connection.on('open', function(){
-    mongoose.connection.db.listCollections(function(error, names) {
-      if (error) {
-        throw new Error(error);
-      } else {
-        names.map(function(name) {
-          console.log('found collection %s', name);
-        });
-      }
-    });
+mongoose.connection.on('open', function () {
+  mongoose.connection.db.listCollections(function (error, names) {
+    if (error) {
+      throw new Error(error);
+    } else {
+      names.map(function (name) {
+        console.log('found collection %s', name);
+      });
+    }
   });
-  
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
